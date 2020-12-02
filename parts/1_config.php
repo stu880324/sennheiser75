b41e376bbd01b1178cf780ed78a969b3f5502066
ZI-YIJIANG <nahcy8114@gmail.com>
2020-12-02 20:32:58 +0800

    Update 1_config.php

--- parts/1_config.php
+++ parts/1_config.php
@@ -11,9 +11,9 @@ if (!isset($_SESSION)) {
 }
 
 $db_host = 'localhost';
-$db_name = 'project57';
-$db_user = 'kim';
-$db_pass = 'adobe30';
+$db_name = 'sennheiser75';
+$db_user = 'root';
+$db_pass = '';
 
 $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8";
 $pdo_options = [
