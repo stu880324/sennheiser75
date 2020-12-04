c48ecd75d6de38b433668ddbf58b5584d69f78fe
Meng-chung <peggy_930@hotmail.com>
2020-12-04 21:19:44 +0800

    navbar js 語法 id-name錯誤更正

--- parts/3_navbar.php
+++ parts/3_navbar.php
@@ -287,9 +287,9 @@
   window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-      document.getElementById("#navbar").style.top = "0";
+      document.getElementById("navbar").style.top = "0";
     } else {
-      document.getElementById("#navbar").style.top = "-200px";
+      document.getElementById("navbar").style.top = "-200px";
     }
     prevScrollpos = currentScrollPos;
   }
